dc2027e7a7412677e21d8cc3cdab5a6bc5a345be
Yanhui Bian <fromthefox@icloud.com>
2025-09-03 19:08:45 +0800

    Update the inference framework and main framework for server

---
+++ model_inference_framework.py
@@ -0,0 +1,63 @@
+"""
+this file is used to finish the inference task
+"""
+
+import model_inference_module
+from model_inference_module import QKV_distribution
+import threading
+import torch
+from model_inference_module import inference_server, dynamic_weights_dis, proportinal_allocation_dis, total_score_dis, cal_new_base_weights, cal_new_dynamic_ratio
+
+def generation_loop(initial_input, max_tokens_length, model, tokenizer, config, server, allocation_list, user_config, dynamic_part, nodes_info_dict):
+    """
+    Generation loop for autocontinuation
+    :param initial_input: initial input text
+    :param max_tokens_length: maximum number of tokens to generate (read from user_config)
+    :returns: the full generated text
+    """
+    current_input = initial_input
+    generated_tokens = 0
+    full_output = ""
+
+    while True:
+        # Perform single-step reasoning
+        
+        res = inference_server(
+            model=model,
+            tokenizer=tokenizer,
+            config=config,
+            server=server,
+            input_text=current_input,
+            allocation_list=allocation_list,
+            user_config=user_config
+        )
+        next_text = res[0]
+        computation_time_list = res[1]
+        translation_time_list = res[2]
+
+        print(f"Generated text: {next_text}")
+        print(f"Computation time: {computation_time_list}")
+        print(f"Translation time: {translation_time_list}")
+        
+        # Update Status
+        full_output += next_text
+        generated_tokens += 1
+        current_input += next_text  # Splicing the newly generated text
+        
+        # Stop condition detection
+        stop_conditions = [
+            generated_tokens >= max_tokens_length,          # Exceeds maximum length
+            next_text in ["</s>", "<|endoftext|>"]   # Common terminators (adjusted to the actual tokenizer)
+        ]
+        
+        if any(stop_conditions):
+            break
+
+        # new_base_weights = cal_new_base_weights(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+        # new_dynamic_ratio = cal_new_dynamic_ratio(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+        # dynamic_weights_array = dynamic_weights_dis(dynamic_weights=dynamic_part, base_weights=new_base_weights)
+        # scores_list = total_score_dis(nodes_info_dict, dynamic_weights_array)
+        # # here 128 is the unsplitted dim of the model.
+        # allocation_list = proportinal_allocation_dis(scores_list, 128)
+
+    return full_output
---
+++ model_inference_main_for_server.py
@@ -0,0 +1,64 @@
+"""
+the whole framework of the project
+here we accept the param from Auto-Deployment Proj.
+:param accepted: list, like [25, 48, 55] for unsplitted-dim == 128
+"""
+import init
+import socket_server
+import threading
+import model_inference_framework
+import numpy as np
+
+
+def infenerce_main_for_server(allocation_list:list, model_path:str, tokenizer_path:str, config_path:str, user_config_path:str, dynamic_part:np.ndarray, nodes_info_dict:dict) -> str:
+    """
+    the whole framework of the project.
+    :parma allocation_list: list, like [25, 48, 55] for unsplitted-dim == 128
+    :parma config_path: str, the path to config file
+    :return: None, just do the inference
+    """
+
+    # 1.
+    
+    # 1.1. init the server
+    server = socket_server.TCPServer(port=44444)
+
+    # 1.2 start listening
+    server_thread = threading.Thread(target=server.start)
+    server_thread.start()
+
+    # 2. load model, tokenizer, config
+    model, tokenizer, config = init.load_model(model_path, tokenizer_path, config_path)
+    user_config_dict = init.parse_ini_file(user_config_path)
+
+    # 3. start the inference
+
+    # 3.1. get the input text and max token
+    input_text = user_config_dict["user_config"]["input_text"]
+    max_token_length = user_config_dict["user_config"]["max_token_length"]
+
+    # 3.2. start inference
+    full_output = model_inference_framework.generation_loop(
+        initial_input=input_text,
+        max_tokens_length=max_token_length,
+        model=model,
+        tokenizer=tokenizer,
+        config=config,
+        server=server,
+        allocation_list=allocation_list,
+        user_config=user_config_dict,
+        dynamic_part=dynamic_part,
+        nodes_info_dict=nodes_info_dict
+    )
+
+    return full_output
+
+infenerce_main_for_server(
+    allocation_list=[47, 47, 17, 17],
+    model_path=r"C:\Users\yhbia\Desktop\学校\∞-杂乱\Llama_3.2_1B\consolidated.00.pth",
+    tokenizer_path=r"C:\Users\yhbia\Desktop\学校\∞-杂乱\Llama_3.2_1B\tokenizer.model",
+    config_path=r"C:\Users\yhbia\Desktop\学校\∞-杂乱\Llama_3.2_1B\params.json",
+    user_config_path=r"C:\Users\yhbia\Desktop\学校\25.6.30-边彦晖-毕业设计\auto_deployment_summary\LLM_Auto_Deployment\Distributed_Llama_Py\user_config.ini",
+    dynamic_part=np.array([0.5, 0.4, 0.1]),
+    nodes_info_dict={'arithmetic': [603, 603, 2301, 2301], 'memory': [8, 8, 350, 350], 'bandwidth': [75, 75, 64, 64]}
+)
